b9a6e24f302557a9245fdc6dc476f44cf8d94da3
david <egbunumalikdavid@gmail.com>
2025-09-14 06:33:13 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php
+// includes/footer.php
+?>
+</main>
+<footer class="footer">
+  <div class="container">
+    <center><p>&copy; <?php echo date('Y'); ?> Valid Crown Internation College, Bwari</p></center>
+  </div>
+</footer>
+</body>
+</html>
+ 
