23cda200e285468c7df920495ec1798209014cc5
david <egbunumalikdavid@gmail.com>
2025-09-14 06:33:16 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php
+//header.php/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>School Details</title>
+  <link rel="stylesheet" href="styles.css"/>
+</head>
+<body>
+<header class="topbar">
+  <div class="container">
+     <center> <h1>Valid Crown Internation College, Bwari</h1></center>
+    <nav class="nav">
+      <a href="index.php">Dashboard</a>
+      <a href="liststudents.php">Students</a>
+      <a href="list.php">Teachers</a>
+      <a href="listadmins.php">Admins</a>
+    </nav>
+  </div>
+</header>
+<main class="container">
